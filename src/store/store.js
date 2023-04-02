b8ae72cff8041c6795c5444fdfd569be003e760b
pankajdp23 <pankaj.patil@excellarate.com>
2023-04-02 14:21:19 +0530

    middleware code

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,26 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 // import counterReducer from "../reducers/postsReducer";
 import postsReducer from "../reducers/postsReducer";
 import usersReducer from "../reducers/usersReducer";
 import albumsReducers from "../reducers/albumsReducers";
 import { todosReducer } from "../reducers/todosReducer";
+
+const middleware = (store) => (next) => (action) => {
+  action.payload = action.payload.slice(0, 50);
+  console.log("action", action);
+  next(action);
+};
+
+/*
+const middleware = function(store) {
+  return function(next) {
+    return function(action) {
+      next(action);
+    }
+  }
+}*/
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   combineReducers({
@@ -12,7 +29,8 @@ const store = createStore(
     albums: albumsReducers,
     todos: todosReducer,
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(middleware))
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 /*
 store.subscribe(() => {
